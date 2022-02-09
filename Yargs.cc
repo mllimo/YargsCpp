2ae1353942c1f83b79f9a72c74872cc0931300bb
mllimo <ar.guijarro.contreras@gmail.com>
2022-02-09 17:33:47 +0000

    Superando todos los tests

--- Yargs.cc
+++ Yargs.cc
@@ -27,11 +27,11 @@ Yargs& Yargs::Parse() {
         if (std::regex_search(argv_[j], std::regex("--|-"))) break;
         values.push_back(argv_[j]);
       }
-      values_[cleaned] = values;
+      values_[alias] = values;
     } else if (bool_keys_.find(alias) != bool_keys_.end()) {
-      values_[cleaned] = true; 
+      values_[alias] = true; 
     } else if (HaveValue(argv_[i])) {
-      values_[cleaned] = GetValue(argv_[i]); 
+      values_[alias] = GetValue(argv_[i]); 
     }  
   }
 
@@ -39,35 +39,40 @@ Yargs& Yargs::Parse() {
 }
 
 
-int Yargs::operator()(const std::string& key, int t) { 
-  return std::any_cast<int>(values_[key]);
+int Yargs::operator()(const std::string& key, int t) {
+  argc::Alias alias(GetAllAlias(key));
+  return std::any_cast<int>(values_[alias]);
 }
 
 
 bool Yargs::operator()(const std::string& key, bool t) {
-  if (values_.find(key) == values_.end()) return false;
+  argc::Alias alias(GetAllAlias(key));
+  if (values_.find(alias) == values_.end()) return false;
   return std::any_cast<bool>(values_[key]);
 }
 
 
 double Yargs::operator()(const std::string& key, double t) {
-  return std::any_cast<double>(values_[key]);
+  argc::Alias alias(GetAllAlias(key));
+  return std::any_cast<double>(values_[alias]);
 }
 
 
 std::string Yargs::operator()(const std::string& key,
                               const std::string& t) {
-  return std::any_cast<std::string>(values_[key]);
+  argc::Alias alias(GetAllAlias(key));
+  return std::any_cast<std::string>(values_[alias]);
 }
 
 
 std::vector<std::string> Yargs::operator()(const std::string& key,
                                            const std::vector<std::string>& t) {
-  return std::any_cast<std::vector<std::string>>(values_[key]);
+  argc::Alias alias(GetAllAlias(key));
+  return std::any_cast<std::vector<std::string>>(values_[alias]);
 }
 
 
-Yargs& Yargs::Array(const std::string& key) { 
+Yargs& Yargs::Array(const std::string& key) {
   array_keys_.insert(key);
   return *this;
 }
